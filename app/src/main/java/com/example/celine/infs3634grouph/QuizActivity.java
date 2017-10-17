046638f9d9f5d9c1571110a44b84c2c952796338
celine1013 <celineliu1010@gmail.com>
2017-10-17 16:21:37 +1100

    quiz done

--- app/src/main/java/com/example/celine/infs3634grouph/QuizActivity.java
+++ app/src/main/java/com/example/celine/infs3634grouph/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.celine.infs3634grouph;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Currency;
 import java.util.List;
@@ -28,6 +30,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_answerB;
     private Button btn_answerC;
     private Button btn_answerD;
+    
     private Button next_question;
     private TextView show_result;
     private Button btn_tryAgain;
@@ -38,7 +41,11 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private int correctNum;
     private int currentCorrect;
     private int currentNum = 0;
-
+    private List<Question> questions;
+    private String[] options;
+    private int scoreTotal = 0;
+    private int curSocre = 10;
+    private Question q;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,93 +72,45 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         }
         // show category
         show_category.setText(getResources().getString(category));
-
+        db = new DatabaseHelper(this);
         final List<Question> questions = db.getQuestionsByCategory(category);
-
-        // show the first question and answer options
-        //maxQuestionNum = questions.size();
-        //说实话不懂这个get(0);
-        Question q = questions.get(0);
-        currentCorrect = q.getTrueAnswer();
-        // show question number
-        show_questionNumber.setText(q.getQuestionID());
-        // get questions using category data sent by main activity
-        show_question.setText(q.getContent());
-
-
-        // TODO: show answer options???????? 这个要把String[]的东西按照[0][1][2][3]的方式放进去，但是试了好多不知道怎么办了
-        btn_answerA.setText(options[0]);
-        btn_answerB.setText(q.getAnswerOptions(<1>));
-        btn_answerC.setText(q.getAnswerOptions());
-        btn_answerD.setText(q.getAnswerOptions());
+        
 
         // calculate score, change the text field
-        int score = 0;
-        show_score.setText(score);
+        show_score.setText(scoreTotal);
 
 
-
-        // calculate and show result
-        // verify if the user has chose the correct answer
-        // if incorrect, show the correct one immediately
-        // TODO: Button变颜色的不知道怎么弄
-        // 按我的逻辑是if((answerChose-1).equals(q.getTrueAnswer)){
         btn_answerA.setOnClickListener(this);
         btn_answerB.setOnClickListener(this);
         btn_answerC.setOnClickListener(this);
         btn_answerD.setOnClickListener(this);
-
-
-
-
 
         next_question.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //如果(answerChose-1).equals(q.getTrueAnswer)就是对的，然后score = score + 10
                 //show_score.setText(score)
-                if(currentNum + 1 < questions.length){
+                if(currentNum < questions.size()){
                     currentNum ++;
+                    showQuestion();
+                    show_score.setText(scoreTotal);
+                    
                 }else{
                     currentNum = 0;
+                    showresult();
                 }
                 //这个get(1）肯定不对不知道咋弄
-                Question q = questions.get(1);
-                show_question.setText(q.getContent());
+                
             }
         });
 
-
-        // change layout
-        setContentView(R.layout.activity_result);
-        // bind new layout
-        show_result = (TextView) findViewById(R.id.showResult);
-        btn_tryAgain = (Button)findViewById(R.id.btnTryAgain);
-        btn_tryAnother = (Button)findViewById(R.id.btnTryAnother);
-
-        // show final score, etc.
-        show_result.setText(score);
-        // btn: finish->go back to main activity;
-        btn_tryAnother.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        // btn: again->start the quiz again
-        btn_tryAgain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change layout
-                setContentView(R.layout.activity_quiz);
-                //TODO:找到此时的Category，然后重新建立所有textView
-            }
-        });
+        
     }
 
     @Override
     public void onClick(View view) {
         int answerChose = -1;
+        int correctButton = 0;
         switch (view.getId()) {
             case R.id.btnAnswerA:
                 answerChose = 1;
@@ -166,15 +125,96 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
                 answerChose = 4;
                 break;
             default:
-                // TODO: 7/10/2017 veryfy if answer chosed match currentAnswer
-
+                // 7/10/2017 veryfy if answer chosed match currentAnswer
                 //if matched, currentCorrect increase, show correct notification, score changed
                 //if mismatched, show incorrect notification
-
+                // 按我的逻辑是if((answerChose-1).equals(q.getTrueAnswer)){
+                if(answerChose == currentCorrect){
+                    correctNum++;
+                    scoreTotal += curSocre;
+                    correctButton = currentCorrect;
+                    Toast.makeText(QuizActivity.this, getResources().getString(R.string.correct_notification), Toast.LENGTH_SHORT);
+                }else{
+                    Toast.makeText(QuizActivity.this, getResources().getString(R.string.incorrect_notification), Toast.LENGTH_SHORT);
+                }
                 //show true answer no matter whether the user answer correctly
                 //true answer btn's background color turns green, others turn green
                 //score change
 
+                switch (currentCorrect){
+                    case 1:
+                        correctButton = R.id.btnAnswerA;
+                        break;
+                    case 2:
+                        correctButton = R.id.btnAnswerB;
+                        break;
+                    case 3:
+                        correctButton = R.id.btnAnswerC;
+                        break;
+                    case 4:
+                        correctButton = R.id.btnAnswerD;
+                        break;
+                }
+                // Button变颜色的不知道怎么弄
+                btn_answerA.setBackgroundColor(getColor(R.color.colorRed));
+                btn_answerB.setBackgroundColor(getColor(R.color.colorRed));
+                btn_answerC.setBackgroundColor(getColor(R.color.colorRed));
+                btn_answerD.setBackgroundColor(getColor(R.color.colorRed));
+                Button btn_choose = (Button)findViewById(correctButton);
+                btn_choose.setBackgroundColor(getColor(R.color.colorGreen));
+
         }
+    }
+    
+    private void showQuestion(){
+        // show the first question and answer options
+        //maxQuestionNum = questions.size();
+        //说实话不懂这个get(0);
+        q = questions.get(currentNum);
+        currentCorrect = q.getTrueAnswer();
+        // show question number
+        show_questionNumber.setText(currentNum);
+        // get questions using category data sent by main activity
+        show_question.setText(q.getContent());
+
+        options = q.getAnswerOptions();
+        // show answer options???????? 这个要把String[]的东西按照[0][1][2][3]的方式放进去，但是试了好多不知道怎么办了
+        btn_answerA.setText(options[0]);
+        btn_answerB.setText(options[1]);
+        btn_answerC.setText(options[2]);
+        btn_answerD.setText(options[3]);
+    }
+    
+    private void showresult(){
+        // change layout
+        setContentView(R.layout.activity_result);
+        // bind new layout
+        show_result = (TextView) findViewById(R.id.showResult);
+        btn_tryAgain = (Button)findViewById(R.id.btnTryAgain);
+        btn_tryAnother = (Button)findViewById(R.id.btnTryAnother);
+
+        // show final score, etc.
+        // TODO: 17/10/2017 change the layout of result: score, correct num
+        show_result.setText(scoreTotal);
+        // btn: finish->go back to main activity;
+        btn_tryAnother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        // btn: again->start the quiz again
+        btn_tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // change layout
+                setContentView(R.layout.activity_quiz);
+                //找到此时的Category，然后重新建立所有textView
+                /*Intent intent = getIntent();
+                finish();
+                startActivity(intent);*/
+                QuizActivity.this.recreate();
+            }
+        });
     }
 }
