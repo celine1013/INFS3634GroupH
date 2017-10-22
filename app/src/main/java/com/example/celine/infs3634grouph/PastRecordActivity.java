f40aeafd0a170ed1c16a2449dbbf87d0883fc7c2
celine1013 <celineliu1010@gmail.com>
2017-10-22 14:48:45 +1100

    recycler layout done

--- app/src/main/java/com/example/celine/infs3634grouph/PastRecordActivity.java
+++ app/src/main/java/com/example/celine/infs3634grouph/PastRecordActivity.java
@@ -2,12 +2,15 @@ package com.example.celine.infs3634grouph;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class PastRecordActivity extends AppCompatActivity {
-
+    private RecyclerView rv_history;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_record);
+        rv_history = findViewById(R.id.rv_history);
+
     }
 }
---
+++ app/src/main/java/com/example/celine/infs3634grouph/RecordAdapter.java
@@ -0,0 +1,35 @@
+package com.example.celine.infs3634grouph;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Celine on 22/10/2017.
+ */
+
+public class RecordAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    public static class RecordViewHolder extends RecyclerView.ViewHolder {
+
+
+        public RecordViewHolder(View itemView) {
+            super(itemView);
+
+        }
+    }
+}
